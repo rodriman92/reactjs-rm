41385693e8af31312755cd2de3d008c5dc3cf462
Rodrigo Mancilla <rodmancilla92@gmail.com>
2022-05-17 21:12:09 -0300

    add styles and navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './styles.css';
+import { NavBar } from './components/NavBar.js';
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Bienvenidos al sitio</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Por qué utilizar React JS?</h3>
-        <ul>
-          <li>Seguridad</li>
-          <li>Performance</li>
-          <li>Escalabilidad</li>
-        </ul>
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React haciendo click aquí
-        </a>
-      </header>
+      <NavBar fixed="top"/>
     </div>
   );
 }
